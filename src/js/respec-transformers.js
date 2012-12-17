c1716b62b0c022b8836791ec310664f19f3fac8e
James Craig <jcraig@apple.com>
2012-12-17 11:17:59 -0800

    part of ACTION-37: Add @ui-actions element and explain event.receiver as opposed to event.target and element registered as the event listener

--- src/js/respec-transformers.js
+++ src/js/respec-transformers.js
@@ -1,12 +1,17 @@
-/* listEvents: alphabetically list of event listeners, generated from markup */
-function listEvents(r, content) {
-	var s = '', eventList = [], nodeList = $$('code.event');
+
+function events() {
+	var eventList = [], nodeList = $$('code.event');
 	for (index in nodeList) {
 		var title = nodeList[index].innerText;
 		if ($$('#'+title).length) eventList.push(title);
 	}
-	s += '<ul>'
-	for (index in eventList.sort()){
+	return eventList.sort();
+}
+
+/* listEvents: alphabetical list of event listeners, generated from markup e.g. 'collapserequest, deleterequest, ...' */
+function listEvents(r, content) {
+	var s = '<ul>', eventList = events();
+	for (index in eventList){
 		var title = eventList[index];
 		var description = '';
 		var els = $$('#'+title+'+dd>p'); // get the paragraph children of the dd that follows the dt event element.
@@ -16,3 +21,15 @@ function listEvents(r, content) {
 	s += '</ul>'
 	return content + s;
 }
+
+/* listActions: alphabetical list generated from trimmed event names. e.g. 'collapserequest, deleterequest, ...' becomes 'collapse, delete, ...' */
+function listActions(r, content) {
+	var s = '<ul>', eventList = events();
+	for (index in eventList){
+		var title = eventList[index];
+		s += '<li><code>' +title.replace('request','')+ '</code></li>';
+	}
+	s += '</ul>'
+	return content + s;
+}
+
